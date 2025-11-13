47a98abfd80820cd425cf01bb41c5fede4117f3a
chadrackmbupiokajess <chadrackmbupiokajess@gmail.com>
2025-11-13 22:56:06 +0100

    Ajout de l'URL pour le sitemap

--- techlearnjess/urls.py
+++ techlearnjess/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
+from core.views import sitemap_xml, robots_txt
 
 admin.site.site_header = "Administration Jessna TechLearn"
 admin.site.site_title = "Portail d'administration Jessna TechLearn"
@@ -12,6 +13,11 @@ admin.site.index_title = "Bienvenue sur le portail d'administration"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    
+    # SEO files
+    path('sitemap.xml', sitemap_xml, name='sitemap_xml'),
+    path('robots.txt', robots_txt, name='robots_txt'),
+
     path('', include('core.urls')),
     path('comptes/', include('accounts.urls')),
     path('cours/', include('courses.urls')),
