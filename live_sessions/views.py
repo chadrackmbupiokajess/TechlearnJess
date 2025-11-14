3767cbf27d4d87d8b37541d3e54e0cd724faa69b
chadrackmbupiokajess <chadrackmbupiokajess@gmail.com>
2025-11-14 20:53:51 +0100

    ajout de test de erreur 500

--- live_sessions/views.py
+++ live_sessions/views.py
@@ -47,6 +47,7 @@ def session_list(request):
 
 def session_detail(request, session_id):
     """Détail d'une session live"""
+    test = 0
     settings = SiteSettings.get_settings()
     session = get_object_or_404(LiveSession, session_id=session_id)
     
